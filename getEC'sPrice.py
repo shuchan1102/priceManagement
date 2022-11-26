d5bc8b9940dd121d7b3c56e13bd97c1c2ca2dcb0
shuchan1102 <miyauchishutaro@gmail.com>
2022-11-27 03:03:19 +0900

    修正

--- getEC'sPrice.py
+++ getEC'sPrice.py
@@ -1,19 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys as keys
 from selenium.webdriver.common.action_chains import ActionChains
-import time
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome import service
-import statistics
 
+import time
+import statistics
 
 def amazonPriceCalc(url,procuctNames):
     #ChromeDriverのテンプレ
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.implicitly_wait(10)
     driver.get(url)
-    #driver.get("https://www.amazon.co.jp/?&tag=hydraamazonav-22&ref=pd_sl_7ibq2d37on_e&adgrpid=56100363354&hvpone=&hvptwo=&hvadid=592007363477&hvpos=&hvnetw=g&hvrand=12997277570780732599&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1009239&hvtargid=kwd-10573980&hydadcr=27922_14541005&gclid=EAIaIQobChMIy4Pk0dG5-wIVElRgCh3QNwNdEAAYASAAEgIDdvD_BwE")
     #2秒待機
     time.sleep(2)
 
---
+++ mainEditedPriceCsv.py
@@ -0,0 +1,63 @@
+import getAllPrice
+import sys
+
+#CSVファイルから対象商品名を読み込み
+#ECサイトの相場を取得
+#出品サイトの相場を取得
+#その結果をCSVファイルに出力する
+
+#入力ファイルからCSV結果の出力先パスと商品名を取得する
+#入力ファイルの形式は、出力ファイルパス名^商品名1^商品名2～～であること
+
+try:
+    f = open('targetProduct.txt', 'r',encoding = "UTF-8")
+    data = f.read()
+except:
+    #ファイルが見つからない場合や入力ファイルに異常がある場合は処理を終了する
+    sys.exit()
+
+#入力ファイルを"^"区切りで分割し商品はリストに、出力パスはそれぞれ変数に格納する
+#また全角スペースが入力ファイルから読み込んだ際に\u3000になってしまうため、置換処理を行う
+splitword = data.split("^")
+product_Names = []
+
+#1ループ目は出力ファイルのパスのため制御する
+loopCount = 0
+
+for word in splitword:
+    if (loopCount == 0):
+        outputFileDir = word
+        loopCount +=1
+        continue  
+    product_Names.append(word.replace("\u3000"," "))
+
+#出力先のディレクトリ
+fileName = "\全商品平均値.csv"
+
+filePath = outputFileDir + fileName
+
+first = ""
+for product_Name in product_Names:
+    first = first + ","+ product_Name
+
+allEcPrice = getAllPrice.getAllEcPrice(product_Names)
+allAuctionPrice = getAllPrice.getAllAuctionPrice(product_Names)
+
+second = ""
+third =""
+fourth =""
+with open(filePath, 'w') as f:
+    editKey = ""
+    for product_Name in product_Names:
+        editKey = product_Name.replace("　","\u3000")
+        second = second + "," + str(allEcPrice[0][editKey])
+        third = third + "," + str(allAuctionPrice[0][editKey])
+        fourth = fourth + "," + str(allAuctionPrice[1][editKey])
+
+        
+    f.write("サイト名"+first+"\n")
+    f.write("amazon"+second+"\n")
+    f.write("メルカリ"+third+"\n")
+    f.write("paypay"+fourth+"\n")
+                    
+
